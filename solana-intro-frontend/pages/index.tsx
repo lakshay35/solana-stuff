a3a3c59b07c31f35a273ca3f2342f92c67cca7fb
Lakshay Sharma <lakshay35@gmail.com>
2023-08-24 22:26:00 -0400

    concluding frontend program interaction and data querying

--- solana-intro-frontend/pages/index.tsx
+++ solana-intro-frontend/pages/index.tsx
@@ -1,38 +1,33 @@
-import * as Web3 from "@solana/web3.js";
-import type { NextPage } from "next";
-import { useState } from "react";
-import AddressForm from "../components/AddressForm";
-import styles from "../styles/Home.module.css";
+
+import { Center, Box, Heading } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { AppBar } from '../components/AppBar'
+import { StudentIntroList } from '../components/StudentIntroList'
+import { Form } from '../components/Form'
+import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  const [balance, setBalance] = useState(0);
-  const [address, setAddress] = useState("");
-
-  const addressSubmittedHandler = (address: string) => {
-    try {
-      setAddress(address);
-      const key = new Web3.PublicKey(address);
-      const connection = new Web3.Connection(Web3.clusterApiUrl("devnet"));
-      connection.getBalance(key).then((balance) => {
-        setBalance(balance / Web3.LAMPORTS_PER_SOL);
-      });
-    } catch (error) {
-      setAddress("");
-      setBalance(0);
-      alert(error);
-    }
-  };
-
   return (
     <div className={styles.App}>
-      <header className={styles.AppHeader}>
-        <p>Start Your Solana Journey</p>
-        <AddressForm handler={addressSubmittedHandler} />
-        <p>{`Address: ${address}`}</p>
-        <p>{`Balance: ${balance} SOL`}</p>
-      </header>
+      <Head>
+        <title>Student Intros</title>
+      </Head>
+      <AppBar />
+      <Center>
+        <Box>
+          <Heading as="h1" size="l" color="white" ml={4} mt={8}>
+            Introduce Yourself!
+          </Heading>
+          <Form />
+          <Heading as="h1" size="l" color="white" ml={4} mt={8}>
+            Meet the Students!
+          </Heading>
+          <StudentIntroList />
+        </Box>
+      </Center>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
